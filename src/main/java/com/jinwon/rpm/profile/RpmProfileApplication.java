7ba3b59e518888b4ff2c46113c2646ef04e71efd
이진원 <ljw0829@midasit.com>
2022-06-18 16:34:08 +0900

    - feat : rpm-profile 초기 세팅

---
+++ src/main/java/com/jinwon/rpm/profile/RpmProfileApplication.java
@@ -0,0 +1,13 @@
+package com.jinwon.rpm.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpmProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpmProfileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jinwon/rpm/profile/RpmProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinwon.rpm.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpmProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
