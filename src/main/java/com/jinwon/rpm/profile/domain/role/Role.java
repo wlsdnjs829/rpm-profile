7012f1510368bf9d462e97ff55319df02df44bc5
ljw0829 <olo_jinwon@naver.com>
2022-06-23 11:25:48 +0900

    - feat : role 복합 컬럼 unique 및 foreign key name 지정

--- src/main/java/com/jinwon/rpm/profile/domain/role/Role.java
+++ src/main/java/com/jinwon/rpm/profile/domain/role/Role.java
@@ -12,14 +12,21 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
-@Table
+@Table(uniqueConstraints = {
+        @UniqueConstraint(
+                name = "profile_role_type_unique_001",
+                columnNames = {"profile_id", "roleType"}
+        )
+})
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -29,8 +36,8 @@ public class Role extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long roleId;
 
-    @JoinColumn(name = "profile_id", nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_id", nullable = false, foreignKey = @ForeignKey(name = "role_profile_foreign_key_001"))
     private Profile profile;
 
     @Column(nullable = false)
